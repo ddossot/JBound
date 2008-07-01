b1203d47ec7f6ab57e8ec6429b2495d662588c75
ddossot <ddossot@fbf1ad31-3caa-4afa-b5b4-094426959cba>
2008-07-01 22:00:49 +0000

    Added extreme values testing in setters and constructors

--- src/main/java/org/jbound/api/JBound.java
+++ src/main/java/org/jbound/api/JBound.java
@@ -3,7 +3,7 @@ package org.jbound.api;
 /**
  * @author David Dossot (david@dossot.net)
  */
-public class JBound {
+public final class JBound {
 
 	public static void run(final Exercises exercises) {
 		exercises.run();
---
+++ src/test/java/org/jbound/subject/ImmutableBean.java
@@ -0,0 +1,83 @@
+package org.jbound.subject;
+
+import java.util.List;
+
+/**
+ * @author David Dossot (david@dossot.net)
+ */
+public class ImmutableBean {
+
+	private final String string;
+
+	private final int primitiveInteger;
+
+	private final Integer integer;
+
+	private final List<Long> longs;
+
+	public ImmutableBean(final String string, final int primitiveInteger,
+			final Integer integer, final List<Long> longs) {
+
+		this.string = string;
+		this.primitiveInteger = primitiveInteger;
+		this.integer = integer;
+		this.longs = longs;
+	}
+
+	public String getString() {
+		return string;
+	}
+
+	public int getPrimitiveInteger() {
+		return primitiveInteger;
+	}
+
+	public Integer getInteger() {
+		return integer;
+	}
+
+	public List<Long> getLongs() {
+		return longs;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((integer == null) ? 0 : integer.hashCode());
+		result = prime * result + ((longs == null) ? 0 : longs.hashCode());
+		result = prime * result + primitiveInteger;
+		result = prime * result + ((string == null) ? 0 : string.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final ImmutableBean other = (ImmutableBean) obj;
+		if (integer == null) {
+			if (other.integer != null)
+				return false;
+		} else if (!integer.equals(other.integer))
+			return false;
+		if (longs == null) {
+			if (other.longs != null)
+				return false;
+		} else if (!longs.equals(other.longs))
+			return false;
+		if (primitiveInteger != other.primitiveInteger)
+			return false;
+		if (string == null) {
+			if (other.string != null)
+				return false;
+		} else if (!string.equals(other.string))
+			return false;
+		return true;
+	}
+
+}
