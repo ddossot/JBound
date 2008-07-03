a69200eeb4e38eafe40e3a0901da4d001104b3bf
ddossot <ddossot@fbf1ad31-3caa-4afa-b5b4-094426959cba>
2008-07-03 01:00:07 +0000

    Added support of generic exceptions.

--- jbound-unit/src/main/java/org/jbound/api/Restriction.java
+++ jbound-unit/src/main/java/org/jbound/api/Restriction.java
@@ -4,5 +4,8 @@ package org.jbound.api;
  * @author David Dossot (david@dossot.net)
  */
 public interface Restriction {
-	Restriction skipping(EXERCISE exercise, EXERCISE... additionalExercises);
+    Restriction skipping(EXERCISE exercise, EXERCISE... additionalExercises);
+
+    Restriction acceptingGenericExceptionsFrom(String accessibleSignature,
+            String... additionalAccessibleSignature);
 }
